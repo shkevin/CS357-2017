d1a8d4477452b3c0118f6aabe64503c1871d642d
Kevin <shk3vin@yahoo.com>
2018-02-28 21:22:31 -0700

    completed prefixSum and numbers

--- HW2/homework2.hs
+++ HW2/homework2.hs
@@ -14,7 +14,7 @@ collatz xs = xs !! (last (getIndexOfVal (maximum $ map (length . indCollatz) xs)
 
 --2.2
 haskellFileNames :: [String] -> [String]
-haskellFileNames strings = filter (endsWith) strings
+haskellFileNames strings = filter (endsWith) strings 
 
 --2.3
 select :: (t -> Bool) -> [t] -> [a] -> [a]
@@ -22,11 +22,12 @@ select p xs ys = [ys !! ind | ind <- elemIndices True $ map (p) xs]
 
 --2.4
 prefixSum :: [Int] -> [Int]
-prefixSum = undefined
+prefixSum [] = []
+prefixSum xs = (prefixSum (take (length xs + (-1)) xs)) ++ [(sum xs)]
 
 --2.5
 numbers :: [Int] -> Int
-numbers = undefined
+numbers xs = sum $ zipWith (*) xs (reverse $ take (length xs) (iterate(10*)1))
 
 --2.6
 type Numeral = (Int, [Int])
@@ -34,8 +35,8 @@ type Numeral = (Int, [Int])
 example = (10, [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0])
 
 --2.6 1
-makeLongInt :: Integer -> Int -> Numeral
-makeLongInt = undefined
+-- makeLongInt :: Integer -> Int -> Numeral
+-- makeLongInt n r = (r, [x | x <- (fromInteger n)])
 
 --2.6 2
 evaluateLongInt :: Numeral -> Integer
@@ -52,6 +53,9 @@ addLongInts = undefined
 --2.6 5
 mulLongInts :: Numeral -> Numeral -> Numeral
 mulLongInts = undefined
+
+
+
 
 --User defined functions
 indCollatz :: Int -> [Int]
@@ -74,4 +78,18 @@ getIndexOfVal val count xs
 
 endsWith :: String -> Bool
 endsWith [] = False
-endsWith str = ".hs" `isSuffixOf` str || ".lhs" `isSuffixOf` str
+endsWith str = substring ".hs" str || substring ".lhs" str
+
+substring :: String -> String -> Bool
+substring _ [] = False
+substring str strToCheck
+                       | str `isSuffixOf` strToCheck = True
+                       | head strToCheck == ' ' = substring str (tail strToCheck)
+                       | (head . reverse) strToCheck == ' ' = substring str (take (length strToCheck + (-1)) strToCheck)
+                       | otherwise = False
+
+-- decomposeNum :: Integer -> [Int]
+-- decomposeNum 0 = 0
+-- decomposeNum num = 
+
+-- length of  num = (length $ intersperse ',' "123") + (-2)
