e0bee24b025751cc4cad37cf3dfbd6624e75c0c5
shkevin <shkevin@unm.edu>
2018-03-05 13:49:27 -0700

    adding comment

--- HW2/homework2.hs
+++ HW2/homework2.hs
@@ -8,26 +8,31 @@ NetID: Shkevin
 --No other imports are allowed
 import Data.List
 
---2.1
+-------------------------------------2.1-----------------------------------------
 collatz :: [Int] -> Int
 collatz xs = xs !! (last (getIndexOfVal (maximum $ map (length . indCollatz) xs) 0 (map (length. indCollatz) xs)))
+---------------------------------------------------------------------------------
 
---2.2
+-------------------------------------2.2-----------------------------------------
 haskellFileNames :: [String] -> [String]
 haskellFileNames strings = filter (endsWith) strings 
+---------------------------------------------------------------------------------
 
---2.3
+-------------------------------------2.3-----------------------------------------
 select :: (t -> Bool) -> [t] -> [a] -> [a]
 select p xs ys = [ys !! ind | ind <- elemIndices True $ map (p) xs]
+---------------------------------------------------------------------------------
 
---2.4
+-------------------------------------2.4-----------------------------------------
 prefixSum :: [Int] -> [Int]
 prefixSum [] = []
 prefixSum xs = (prefixSum (take (length xs + (-1)) xs)) ++ [(sum xs)]
+---------------------------------------------------------------------------------
 
---2.5
+-------------------------------------2.5-----------------------------------------
 numbers :: [Int] -> Int
 numbers xs = read $ concatMap (show) xs :: Int
+---------------------------------------------------------------------------------
 
 --2.6
 type Numeral = (Int, [Int])
@@ -35,33 +40,32 @@ type Numeral = (Int, [Int])
 example = (10, [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0])
 
 --2.6 1
--- makeLongInt :: Integer -> Int -> Numeral
--- makeLongInt n r
---                | numLength n 1 == 1 = 
---                | otherwise = head (decomposeNum n) * (r ^ (numLength n 0)) + makeLongInt (n `mod` (last (take (numLength n 1) (iterate (10*)1)))) r
+makeLongInt :: Integer -> Int -> Numeral
+makeLongInt n r = (r, changeBase (fromIntegral n) (toInteger r))
+---------------------------------------------------------------------------------      
 
---2.6 2
--- evaluateLongInt :: Numeral -> Integer
--- evaluateLongInt (_, []) = 0
--- evaluateLongInt (r, l) = toInteger (head l) * (toInteger $ r ^ (length l + (-1))) + evaluateLongInt (r, drop 1 l)
---
+------------------------------------2.6 2----------------------------------------
+evaluateLongInt :: Numeral -> Integer
+evaluateLongInt = undefined
+---------------------------------------------------------------------------------
 
--- --2.6 3
+------------------------------------2.6 3----------------------------------------
 changeRadixLongInt :: Numeral -> Int -> Numeral 
-changeRadixLongInt (_, l) nr = (nr, changeBase (numbers l) nr)
+changeRadixLongInt = undefined
+---------------------------------------------------------------------------------
 
---2.6 4
+------------------------------------2.6 4----------------------------------------
 addLongInts :: Numeral -> Numeral -> Numeral
 addLongInts = undefined
+---------------------------------------------------------------------------------
 
---2.6 5
+------------------------------------2.6 5----------------------------------------
 mulLongInts :: Numeral -> Numeral -> Numeral
 mulLongInts = undefined
+---------------------------------------------------------------------------------
 
 
-
-
---User defined functions
+----------------------------User defined functions-------------------------------
 indCollatz :: Int -> [Int]
 indCollatz 1 = [1]
 indCollatz x
@@ -102,13 +106,13 @@ numLength num iteration
                       | num `div` 10 == 0 = iteration
                       | otherwise = numLength (num `div` 10) (increment iteration)
 
--- test :: [Int] -> Int
-test n r = map (\(x, y) -> x * (r^y)) (zip (decomposeNum n) (generateDecList (numLength n 1 + (-1))))
+-- test :: [Int] -> Int -> Integer
+test n r = map (\(x, y) -> x * (r^y)) (zip (decomposeNum n) (generateDecList ((fromIntegral $ numLength n 0))))
 
-changeBase :: Int -> Int -> [Int]
+changeBase :: Integer -> Integer -> [Int]
 changeBase 0 _ = []
-changeBase n r = changeBase (n `div` r) r ++ [n `mod` r]
+changeBase n r = changeBase (n `div` r) r ++ [fromInteger $ n `mod` r]
 
-generateDecList :: Int -> [Int]
+generateDecList :: Integer -> [Int]
 generateDecList 0 = [0]
-generateDecList x = [x] ++ generateDecList (x +(-1))
+generateDecList x = [fromIntegral x] ++ generateDecList (fromIntegral x +(-1))
