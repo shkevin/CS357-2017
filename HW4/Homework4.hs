6e36b96a60db7689ef6530ac4c0f308bbf4f35b9
shkevin <shk3vin@yahoo.com>
2018-04-12 13:19:28 -0600

    mostly done

--- HW4/Homework4.hs
+++ HW4/Homework4.hs
@@ -12,7 +12,9 @@ import qualified Data.List as L
   * RETURNS:
 -}
 insertions :: String -> [String]
-insertions = undefined
+insertions [] = [['A']] ++ [['T']] ++ [['G']] ++ [['C']]
+insertions (x:xs) = [['A'] ++ (x:xs)] ++ [['T'] ++ (x:xs)] ++ 
+    [['G'] ++ (x:xs)] ++ [['C'] ++ (x:xs)] ++ (map (x:) (insertions xs))
 
 {-
   * PARAMETERS:
@@ -20,7 +22,9 @@ insertions = undefined
   * RETURNS:
 -}
 deletions :: String -> [String]
-deletions = undefined
+deletions "" = [""]
+deletions (x:"") = [""]
+deletions (x:xs) = [xs] ++ (map (x:) (deletions xs))
 
 {-
   * PARAMETERS:
@@ -28,7 +32,13 @@ deletions = undefined
   * RETURNS:
 -}
 substitutions :: String -> [String]
-substitutions = undefined
+substitutions "" = [""]
+substitutions (x:xs) = take ((length $ subs (x:xs)) - 1) (subs (x:xs))
+
+subs :: String -> [String]
+subs "" = [""]
+subs (x:xs) = [['A']++ xs]++[['T']++ xs] ++
+    [['G']++ xs] ++ [['C']++ xs] ++ (map (x:) (subs xs))
 
 {-
   * PARAMETERS:
@@ -36,7 +46,10 @@ substitutions = undefined
   * RETURNS:
 -}
 transpositions :: String -> [String]
-transpositions = undefined
+transpositions "" = [[]]
+transpositions (a:"") = [[a]]
+transpositions (x:y:[]) = [(y:x:[])]
+transpositions (x:y:xs) = [(y:x:xs)] ++ map (x:) (transpositions (y:xs))
 
 -------------------------------4.2 Sorting (20pts)------------------------------
 {-
@@ -46,9 +59,9 @@ transpositions = undefined
 -}
 insert :: Ord a => a -> [a] -> [a]
 insert y [] = [y]
-insert y (x:xs)
-              | y <= x = y:x:xs ++ insert y xs
-              | otherwise = x : insert y xs
+insert x (y:ys)
+              | y <= x = x:y:ys
+              | otherwise = y:insert x ys
 
 {-
   * PARAMETERS: List to be sorted.
@@ -57,7 +70,11 @@ insert y (x:xs)
 -}
 isort :: Ord a => [a] -> [a]
 isort [] = []
-isort xs = take (length xs) (insert (head xs) (isort (tail xs)))
+isort xs = reverse $ (isortHelper xs)
+           where 
+                isortHelper :: Ord a => [a] -> [a]
+                isortHelper [] = []
+                isortHelper (x:xs) = insert x (isortHelper xs)
 
 {-
   * PARAMETERS:
@@ -66,14 +83,20 @@ isort xs = take (length xs) (insert (head xs) (isort (tail xs)))
 -}
 --can use readFile, getContents, words. 
 fileisort :: String -> String -> IO ()
-fileisort = undefined
-
-test = do x <- readFile "D:/ProgramFiles/Shkevin/cs357/HW4/text.txt"
-          putStr x
+fileisort fn1 fn2 = writeFile fn2 . unlines . isort . lines =<< readFile fn1
 -----------------------------4.3 Game Trees (40pts)-----------------------------
 data Field = B | R | G
              deriving (Eq, Ord, Show)
 type Board = [Field]
+
+data Tree a = Node a [Tree a] 
+              deriving Show
+
+size :: Int
+size = 3
+
+depth :: Int
+depth = 9
 
 {-
   * PARAMETERS:
@@ -81,7 +104,7 @@ type Board = [Field]
   * RETURNS:
 -}
 strategyForRed :: Board -> Int
-strategyForRed = undefined
+strategyForRed board = genericStrat board R
 
 {-
   * PARAMETERS:
@@ -89,7 +112,123 @@ strategyForRed = undefined
   * RETURNS:
 -}
 strategyForGreen :: Board -> Int
-strategyForGreen = undefined
+strategyForGreen board = genericStrat board G
+
+genericStrat :: Board -> Field -> Int
+genericStrat board field =  head $ L.elemIndices (True) listOfBools
+                            where listOfBools = help $ zip (concat $ bestmove (splitEvery 3 board) field) board
+
+help :: [(Field, Field)] -> [Bool]
+help [] = [False]
+help (x:xs)
+                    | fst x == snd x = False : help xs
+                    | otherwise = True : help xs
+
+next :: Field -> Field
+next G = R
+next R = G
+next B = B
+
+getWinner :: [Board] -> Field
+getWinner board = if wins G board
+                  then G
+                  else R
+
+empty :: [Board]
+empty = replicate size (replicate size B)
+
+full :: [Board] -> Bool
+full = all (/= B) . concat
+
+turn :: [Board] -> Field
+turn board = if gs <= rs then G else R
+         where
+            rs = length (filter (==R) ps)
+            gs = length (filter (==G) ps)
+            ps = concat board
+
+wins :: Field -> [Board] -> Bool
+wins field board = any line (rows ++ cols ++ dias)
+                   where
+                      line = all (== field)
+                      rows = board
+                      cols = L.transpose board
+                      dias = [diag board, diag (map reverse board)]
+
+diag :: [Board] -> Board
+diag board = [board !! n !! n | n <- [0..size-1]]
+
+splitEvery _ [] = []
+splitEvery n list = first : (splitEvery n rest)
+                    where
+                       (first, rest) = splitAt n list
+
+won :: [Board] -> Bool
+won board = wins G board || wins R board
+
+putBoard :: [Board] -> IO ()
+putBoard =
+          putStrLn . unlines . concat . interleave bar . map showRow
+          where bar = [replicate ((size*4)-1) '-']
+
+showRow :: Board -> [String]
+showRow = beside . interleave bar . map showField
+          where
+             beside = foldr1 (zipWith (++))
+             bar = replicate 3 "|"
+
+showField :: Field -> [String]
+showField R = ["   ", " R ", "   "]
+showField B = ["   ", "   ", "   "]
+showField G = ["   ", " G ", "   "]
+
+interleave :: a -> [a] -> [a]
+interleave x [] = []
+interleave x [y] = [y]
+interleave x (y:ys) = y : x : interleave x ys
+
+valid :: [Board] -> Int -> Bool
+valid board i = 0 <= i && i < size^2 && concat board !! i == B
+
+move :: [Board] -> Int -> Field -> [[Board]]
+move board i field = 
+                   if valid board i then [chop size (xs ++ [field] ++ ys)] else []
+                   where (xs, B:ys) = splitAt i (concat board)
+
+chop :: Int -> [a] -> [[a]]
+chop n [] = []
+chop n xs = take n xs : chop n (drop n xs)
+
+gametree :: [Board] -> Field -> Tree [Board]
+gametree board field = Node board [gametree g' (next field) | g' <- moves board field]
+
+moves :: [Board] -> Field -> [[Board]]
+moves board field
+                 | won board = []
+                 | full board = []
+                 | otherwise = concat [move board i field | i <- [0..((size^2)-1)]]
+
+prune :: Int -> Tree a -> Tree a
+prune 0 (Node x _) = Node x []
+prune n (Node x ts) = Node x [prune (n-1) t | t <- ts]
+
+minimax :: Tree [Board] -> Tree ([Board], Field) 
+minimax (Node board [])                                                     --G == 0, R = X
+                      | wins G board = Node (board, G) []
+                      | wins R board = Node (board, R) []
+                      | otherwise = Node (board, B) []
+minimax (Node board ts)
+                      | turn board == G = Node (board, minimum ps) ts'
+                      | turn board == R = Node (board, maximum ps) ts'
+                                          where
+                                             ts' = map minimax ts
+                                             ps = [p | Node (_,p) _ <- ts']
+
+bestmove :: [Board] -> Field -> [Board]
+bestmove board field = head [g' | Node (g',field') _ <- ts, field' == best]
+                       where
+                          tree = prune depth (gametree board field)
+                          Node (_,best) ts = minimax tree
 
 ------------4.4 (Optional) Drawing Game Trees and Strategies (30pts EC)---------
 {-
